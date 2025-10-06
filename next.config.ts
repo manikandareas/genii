6d276deaf3d07e699f713d156f789c443dcd2d62
Vito Andareas Manik <vitoandareas15@gmail.com>
2025-10-06 15:05:04 +0800

    feat: implement Content Security Policy headers with utterances.js support

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,19 @@
 import type { NextConfig } from "next";
 
+const csp = [
+  "default-src 'self'",
+  "script-src 'self' https://utteranc.es",
+  "frame-src https://utteranc.es", // untuk iframe komentar
+  "img-src 'self' data: https://github.com https://avatars.githubusercontent.com",
+  "connect-src 'self'", // tambahkan origin lain kalau perlu
+  "style-src 'self'", // TETAP tanpa 'unsafe-inline' di halaman kamu
+  "font-src 'self' data:",
+].join("; ");
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "linear.app",
-      },
       {
         protocol: "https",
         hostname: "superb-woodpecker-318.convex.cloud",
@@ -53,6 +59,10 @@ const nextConfig: NextConfig = {
             value: "camera=(), microphone=(), geolocation=()",
           },
         ],
+      },
+      {
+        source: "/(.*)",
+        headers: [{ key: "Content-Security-Policy", value: csp }],
       },
     ];
   },
